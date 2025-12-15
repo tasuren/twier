1f52b5c68efd06b784284338a9322aed2082bbf7
Takagi Tasuku <tasuren@outlook.jp>
2025-12-15 16:18:09 +0900

    fix: update Rspack configuration

--- rspack.config.ts
+++ rspack.config.ts
@@ -1,33 +1,35 @@
-import { Configuration } from "@rspack/cli";
+import type { Configuration } from "@rspack/cli";
 
 const CONFIG: Configuration = {
-  context: __dirname,
-  module: {
-    rules: [
-      {
-        use: [
-          {
-            loader: "builtin:swc-loader",
-            options: {
-              sourceMap: true,
-              jsc: {
-                parser: {
-                  syntax: "typescript",
-                  tsx: false,
-                },
-              },
-            },
-          },
-        ],
-      },
-    ],
-  },
-  output: {
-    filename: "main.js",
-    path: "./dist",
-  },
-  performance: { hints: false },
-  devtool: "inline-source-map",
+	context: __dirname,
+	module: {
+		rules: [
+			{
+				use: [
+					{
+						loader: "builtin:swc-loader",
+						options: {
+							sourceMap: true,
+							jsc: {
+								parser: {
+									syntax: "typescript",
+									tsx: false,
+								},
+							},
+						},
+					},
+				],
+			},
+		],
+	},
+	output: {
+		filename: "main.js",
+	},
+	resolve: {
+		extensions: [".ts"],
+	},
+	performance: { hints: false },
+	devtool: "inline-source-map",
 };
 
 export default CONFIG;
